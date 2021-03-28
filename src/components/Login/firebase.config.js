40e2200aea8010ff6c48f4b64a4af8d9be5f41db
mmoniruzzaman1981 <76765440+mmoniruzzaman1981@users.noreply.github.com>
2021-03-29 00:07:10 +0600

    Update firebase.config.js

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCtSZZtAnNBYs8BTeMYG8avbfLc28926VI",
-    authDomain: "my-burj-al-arab.firebaseapp.com",
-    projectId: "my-burj-al-arab",
-    storageBucket: "my-burj-al-arab.appspot.com",
-    messagingSenderId: "252070721962",
-    appId: "1:252070721962:web:052d6ee613e5034dea93d3"
+   
   };
 
 export default firebaseConfig;
